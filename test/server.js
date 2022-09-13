85a16d9aade076d8a024e6f1d94c089fdd2861d0
Mitchell Wilson <mitchelldw01@gmail.com>
2022-09-12 20:24:00 -0500

    added test folder for example

---
+++ test/server.js
@@ -0,0 +1,10 @@
+import { createServer } from "http";
+
+const server = createServer((_, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.write("Hello, World!");
+  res.end();
+});
+
+server.listen(3000, () => console.log("Server running on port: 3000"));
