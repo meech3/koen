ee8a7a5b61503ead11ae2398b65043331747ea3d
Mitchell Wilson <mitchelldw01@gmail.com>
2022-09-12 19:47:46 -0500

    wrote main script

---
+++ src/index.ts
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+import { watch, readFileSync } from "fs";
+import { ChildProcess, spawn } from "child_process";
+import { resolve, join } from "path";
+
+type Options = {
+  include?: string;
+  exclude?: string;
+  exec?: string;
+};
+
+const args = process.argv.slice(2);
+const dir = getDir(args);
+const opts = parseOpts(args.slice(1));
+
+let child: ChildProcess;
+let command: string[];
+
+if (opts.exec) {
+  const execArgs = opts.exec.split(" ");
+  child = spawn(execArgs[0], execArgs.slice(1), { stdio: "inherit" });
+} else {
+  command = getStartScript();
+  child = spawn(command[0], command.slice(1), { stdio: "inherit" });
+}
+
+let fsWait: ReturnType<typeof setTimeout> | boolean = false;
+
+watch(dir, { recursive: true }, (_, filename) => {
+  if (!filename) return;
+  if (fsWait) return;
+
+  fsWait = setTimeout(() => {
+    fsWait = false;
+  }, 100);
+
+  if (opts.include) {
+    const regex = new RegExp(opts.include);
+    if (!filename.match(regex)) return;
+  } else if (opts.exclude) {
+    const regex = new RegExp(opts.exclude);
+    if (filename.match(regex)) return;
+  }
+
+  restart();
+});
+
+export function getDir(args: string[]): string {
+  if (args[0].startsWith("--")) {
+    args.unshift(resolve("."));
+  }
+  return args[0];
+}
+
+export function parseOpts(opts: string[]): Options {
+  return opts.reduce((acc: Options, curr, i) => {
+    if (!curr.startsWith("--")) return acc;
+
+    if (!(opts.length > i + 1)) {
+      console.error(`error: missing argument for "${curr}"`);
+      process.exit(1);
+    }
+
+    const key = curr.replace("--", "");
+    acc[key as keyof Options] = opts[i + 1];
+
+    return acc;
+  }, {});
+}
+
+export function getStartScript(): string[] {
+  const packageJSON = JSON.parse(readFileSync(join(String(process.env.PWD), "package.json"), "utf-8"));
+
+  if (!packageJSON.scripts.start) {
+    console.error("error: could not determine start script");
+    process.exit(1);
+  }
+
+  return packageJSON.scripts.start.split(" ");
+}
+
+export function restart(): void {
+  if (opts.exec) {
+    const execArgs = opts.exec.split(" ");
+    spawn(execArgs[0], execArgs.slice(1), { stdio: "inherit" });
+  } else {
+    child.kill();
+    setTimeout(() => {
+      child = spawn(command[0], command.slice(1), { stdio: "inherit" });
+    }, 100);
+  }
+}
